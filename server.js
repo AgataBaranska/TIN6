afc50c147ce505ced12ff7e4c932a960cd7792a5
s19487 <s19487@pjwstk.edu.pl>
2021-05-02 23:25:33 +0200

    server.js update1

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //example http://localhost:3000/?operacja=pomnóż&number1=23&number2=3
 const http = require('http');
+const Operation = {
+    'dodaj': '+',
+    'odejmij': '-',
+    'pomnóż': '*',
+    'podziel': '/'
+}
 http.createServer((req, res) => {
     const url = new URL('http://localhost' + req.url);
-    var Operation = {
-        'dodaj': '+',
-        'odejmij': '-',
-        'pomnóż': '*',
-        'podziel': '/'
-    }
+
     var operacja = url.searchParams.get('operacja');
     var number1 = parseFloat(url.searchParams.get('number1'));
     var number2 = parseFloat(url.searchParams.get('number2'));
@@ -25,10 +26,20 @@ http.createServer((req, res) => {
         case 'podziel':
             result = number1 / number2;
             break;
+        default:
+            res.statusMessage = 'Invalid query';
+            res.statusCode = 400;
+            res.end();
+
     }
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
-    });
-    res.write(`${number1} ${Operation[operacja]} ${number2} = ${result}`);
-    res.end();
+    console.log(typeof result);
+    if (typeof result !== 'undefined') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        });
+        res.write(`${number1} ${Operation[operacja]} ${number2} = ${result}`);
+        res.end();
+
+    }
+
 }).listen(3000);
